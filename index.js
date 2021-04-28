3372c091dfafe1d1fcd89d7a00ba366ce634c178
acbewley <acbewley13@gmail.com>
2021-04-28 12:54:06 -0400

    worked on writing functions

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const util = require('util');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+const initHTML = 
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <h1 class="fs-4">Your Company!</h1>
+    </header>
+  </div>
+  </div id="card-here"></div>
+</body>
+</html>`
+
 function promptUser() {
   return inquirer.prompt([
     {
@@ -38,3 +57,9 @@ function promptUser() {
     }
   ])
 }
+
+promptUser()
+  .then(function(answers) {
+    writeFileAsync("index.html", initHTML);
+    
+  })
